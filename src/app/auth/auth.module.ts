ed7ac3d2aac77af46e78a8a7683c32f14fdd9006
Freddy Escobar <frescobar87@gmail.com>
2021-06-27 22:11:29 -0500

    arreglo de rutas

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+   
+    
+  ],
+  exports: [LoginComponent,RegisterComponent]
+})
+export class AuthModule { }
